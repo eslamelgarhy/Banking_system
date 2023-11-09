6522edb79f51dbc8c79bd2e48780daff635219c7
Eslam Elgarhy <93857552+eslamelgarhy@users.noreply.github.com>
2023-11-09 11:06:51 +0200

    Banking_System.py

--- Banking_System.py
+++ Banking_System.py
@@ -7,82 +7,82 @@ class operation:
   
   
   def deposit(self): 
-    amount= int(input("enter the amount you want to deposit :"))
+    amount= int(input("Enter The Amount You Want To Deposit :"))
     self.balance+= amount
-    print(f"Deposit has been made :{amount} \nand your balance is :{self.balance}")
+    print(f"Deposit Has Been Made :{amount} \nAnd Your Balance Is :{self.balance}")
 
   
   def withdraw(self):
-    amount= int(input("enter the amount you want to withdraw :"))
+    amount= int(input("Enter The Amount You Want To Withdraw :"))
     if amount <= self.balance:
         self.balance-= amount
-        print(f"Withdrawn: {amount}\nYour balance is now: {self.balance}")
+        print(f"Withdrawn: {amount}\nYour Balance Now: {self.balance}")
     else:
-            print(f"Insufficient funds. available balance :",self.balancs)
+            print(f"Insufficient Funds. Available Balance :",self.balance)
 
 
 
 class SignUpAndLogIn(operation) :
   
   def sign_up(self):
-    self.name = input("weclome! \nto use this bank enter this information : \nenter your name : ")
-    self.phone = input("enter your phone : ")
-    self.email= input("enter your email : ")
-    self.password =input("enter your password : ")
+    self.name = input("Weclome! \nTo Use This Bank Enter This Information : \nEnter Your Name : ")
+    self.phone = input("Enter Your Phone : ")
+    self.email= input("Enter Your Email : ")
+    self.password =input("Enter Your Password : ")
 
 
   def log_in(self):
-    self.lname= input("To sign in \nenter your name : ")
-    self.lpassword =input("enter your password : ")
+    self.lname= input("To Login \nEnter Your Name : ")
+    self.lpassword =input("Enter Your Password : ")
+  def continue_operation(self):
+        new_ope = input("Did You Want To Do Another Operation :( y / n ) ")
+        if new_ope=="Y" or new_ope=="y" :
+           self.check_login()
+        elif new_ope=="N" or new_ope=="n":
+           print("Thanks For Using Our Bank.")
+        else:
+         print("Invalid Input.")
+         self.continue_operation() 
   def check_login(self):
+    
     if self.name == self.lname and self.password == self.lpassword :
-      ope= input("enter 1- deposit 2- withdraw : ")
+      ope= input("\nEnter\n\n   1-> Deposit   2-> Withdraw   3-> Show Balance: ")
       if ope == "1":
         self.deposit()
-        new_ope = input("did you want to do another operation :( y / n ) ")
-        if new_ope=="y":
-           self.check_login()
-        elif new_ope=="n":
-           print("thanks for use our bank")
-        else:
-         print("Invalid input.") 
+        self.continue_operation()
 
 
       elif ope == "2":
         self.withdraw()
-        new_ope = input("did you want to do another operation :( y / n ) ")
-        if new_ope=="y":
-           self.check_login()
-        elif new_ope=="n":
-           print("thanks for use our bank")
-        else:
-         print("Invalid input.")
-      
+        self.continue_operation()
+
+      elif ope =="3":
+         print("Available Balance In Your Account Is: ",self.balance)
+         self.continue_operation()
       else:
-         print("Invalid input.")
+         print("Invalid Input.")
+         self.continue_operation()
 
 
     else : 
-       print("Name or Password error. try agin ")
-       self.log_in() 
-       self.check_login()
-    
-  def display(self):
+       print("Name Or Password Error. Try Again ")
+       
+class display(SignUpAndLogIn) :  
+  
+  def action(self):
     while True:
 
-        display = input("enter 1 - To log In 2 - show balance , 3 - To quit :")
+        display = input("\nEnter\n\n   1-> To log In     2-> To Quit :")
         if display == "1":
            self.log_in()
            self.check_login()
         elif display=="2":
-          print("available balance is : ",self.balance)
-        elif display == "3":
-            print("Thanks for using our bank.")
+            print("Thanks For Using Our Bank.")
             break
         else:
-            print("Invalid input.")
+            print("Invalid Input.")
 
 
-sign = SignUpAndLogIn()
+sign = display()
 sign.sign_up()
-sign.display()
+sign.action()
